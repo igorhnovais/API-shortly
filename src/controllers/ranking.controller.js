374816536f5a7288c0cf545051cbad5355e5e453
Igor Novais <igorhnovaisdriven@gmail.com>
2022-12-19 15:16:43 -0300

    feat: getRanking

--- src/controllers/ranking.controller.js
+++ src/controllers/ranking.controller.js
@@ -2,4 +2,21 @@ import { connection } from "../database/db.js";
 
 export async function getRanking(req,res){
 
+    const ranking = await connection.query(`
+        SELECT us.id, us.name, 
+        CASE WHEN COUNT(ur.*) = 0 THEN 0
+        ELSE COUNT(ur.*) 
+        END AS "linksCount", 
+        CASE WHEN COUNT(ur.*) = 0 THEN 0 
+        ELSE SUM(ur.visit_count) 
+        END AS "visitCount" 
+        FROM users AS us 
+        LEFT JOIN urls AS ur 
+        ON ur.user_id = us.id 
+        GROUP BY us.id 
+        ORDER BY "visitCount" DESC, "linksCount" DESC
+        LIMIT 10;`
+    );
+
+    res.send(ranking.rows).status(200);
 };
